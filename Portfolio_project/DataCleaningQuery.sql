9b40649a136fb3b53268c77051c3e02936185db4
Vishud Verma <244vishudverma@gmail.com>
2025-06-11 21:33:38 +0530

    update

---
+++ Portfolio_project/DataCleaningQuery.sql
@@ -0,0 +1,12 @@
+USE Portfolio;
+
+/*
+This file is for data cleaning queries
+all the steps taken in the process are listed one by one
+1. Standardise the date format
+2. Populate the Address data
+3. Breaking out Address into Individual Columns (Address, City, State)
+4. Change Y and N to Yes and No in "Sold as Vacant" column
+5. Remove unused columns
+*/
+
